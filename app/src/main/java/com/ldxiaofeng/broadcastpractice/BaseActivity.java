946baa902283a587b84d485c57f557d5fbf58a35
小枫 <1317847793@qq.com>
2018-06-24 23:27:01 +0800

    增加记住密码

--- app/src/main/java/com/ldxiaofeng/broadcastpractice/BaseActivity.java
+++ app/src/main/java/com/ldxiaofeng/broadcastpractice/BaseActivity.java
@@ -1,6 +1,6 @@
 package com.ldxiaofeng.broadcastpractice;
 
-import android.app.Activity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,7 +9,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 /**
  * Created by 13178 on 2018-6-18.
--- app/src/main/java/com/ldxiaofeng/broadcastpractice/LoginActivity.java
+++ app/src/main/java/com/ldxiaofeng/broadcastpractice/LoginActivity.java
@@ -2,10 +2,13 @@ package com.ldxiaofeng.broadcastpractice;
 
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,26 +17,51 @@ import android.widget.Toast;
  */
 public class LoginActivity extends BaseActivity {
 
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
+
     private EditText accountEdit;
 
     private  EditText passwordEdit;
 
     private Button login;
 
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.login);
+        boolean isRemember =pref.getBoolean("remember_password",false);
+        if(isRemember){
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
                 if (account.equals("admin")&& password.equals("123456")){
+                    editor = pref.edit();
+                    if(rememberPass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.apply();
                     Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
